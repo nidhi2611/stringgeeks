d9820d98df98868965076dfe6de28e629687732e
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-01-17 03:01:27 +0530

    returns the reverse of the string

--- src/reverseString/Reverse.java
+++ src/reverseString/Reverse.java
@@ -1,11 +1,27 @@
 package reverseString;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Reverse {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int testcases = sc.nextInt();
+        while (testcases-- > 0) {
+            int p = 0;
 
+            String str = sc.next();
+            ArrayList<String> list = new ArrayList<>();
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '.') {
+                    list.add(str.substring(p, i));
+                    p = i + 1;
+                }
+
+            }
+            for (int j = list.size() - 1; j >= 0; j--) {
+                System.out.print(list.get(j) + ".");
+            }
+        }
     }
 }
