d73834c01113b029f6653483aa7e35225a23d72e
ishaparveen <nidhi.gupta_cs18@gla.ac.in>
2020-01-17 02:30:58 +0530

    input number of testcase

--- src/reverseString/Reverse.java
+++ src/reverseString/Reverse.java
@@ -1,7 +1,11 @@
 package reverseString;
 
+import java.util.Scanner;
+
 public class Reverse {
     public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int testcases = sc.nextInt();
 
     }
 }
